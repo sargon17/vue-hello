c39d4c03df536dba814c9869e06a9c48d5281c28
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-24 14:16:58 +0100

    done

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: "#root",
+  data: {
+    message: "Hello World",
+    imageLink:
+      "https://images.unsplash.com/photo-1619301616767-f64191c58c5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=708&q=80",
+  },
+});
